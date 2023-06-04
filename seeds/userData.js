d29e8bc08d5c17e0770c9e8a063d86b49be1f55a
taptapclicky <njhonny99@mail.com>
2023-06-03 21:21:58 -0400

    commented out some code

--- seeds/userData.js
+++ seeds/userData.js
@@ -3,23 +3,23 @@ const {User} = require("../models");
 const userData = [
     {
     
-        "name": "John doe",
+        // "name": "John doe",
         "email": "catlover@aol.com",
         "password": "Bigcat125!"
       }, {
-        "name": "Ricky Smith",
+        // "name": "Ricky Smith",
         "email": "rickster5@gmail.com",
         "password": "Therickman?1"
       }, {
-        "name": "timmy Turner",
+        // "name": "timmy Turner",
         "email": "Fairlyodd@gmail.com",
         "password": "Cosmowanda!2"
       }, {
-        "name": "Juju walker",
+        // "name": "Juju walker",
         "email": "spaceman2@yahoo.com",
         "password": "Apollo10!"
       }, {
-        "name": "Bobby Boucher",
+        // "name": "Bobby Boucher",
         "email": "Waterboy@yahoo.com",
         "password": "H20isgood!"
       }
